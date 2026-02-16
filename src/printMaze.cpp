5cff9d2d1b229e491aa551746cdf5a838834891d
ZicoNederhand <ziconederhand@gmail.com>
2026-02-16 15:40:59 +0100

    one comment

--- src/printMaze.cpp
+++ src/printMaze.cpp
@@ -2,11 +2,13 @@
 
 //Define the function with the maze as input. Notice the & symbol to call by reference
 void printMaze(const std::array<std::array<char,12>,12>& maze) {
+    // for every cell in every row display its character and a space for readability
     for (const std::array<char, 12>& row : maze) {
         for (char cell : row) {
             std::cout << cell << ' ';
         }
         std::cout << std::endl;
     }
+    //After each maze add a line to seperate the mazes when traversing
     std::cout << "----------------------" << std::endl;
 }
