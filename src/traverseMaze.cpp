18ab7faffa5b0dc71a0d52816c73ed0b80c42ea0
ZicoNederhand <ziconederhand@gmail.com>
2026-02-18 21:05:05 +0100

    More efficient code I think

--- src/traverseMaze.cpp
+++ src/traverseMaze.cpp
@@ -7,84 +7,73 @@ void traverseMaze(std::array<std::array<char,12>,12>& maze, std::array<int,2> po
     int current_y = position[0];
     //Create boolean to check if the maze is solved.
     bool reachedEnd = 0;
-    //Depending on direction, check to the right, then forward, then left and otherwise go back. 
-    //When direction in the maze is left, then up is to the right of the current direction.
-    //This goes for all directions
-    if (direction == Direction::Left) {
-        //Check if the next spot has a dot(free) or an x(have been here before), to check if we can move there
-        if (maze[current_y-1][current_x]=='.' || maze[current_y-1][current_x]=='x'){
-            //Adjust coordinate to new spot
-            current_y--;
-            //Adjust direction to new direction
-            direction = Direction::Up;
-        } else if (maze[current_y][current_x-1]=='.' || maze[current_y][current_x-1]=='x'){
-            current_x--;
-            direction = Direction::Left;
-        } else if (maze[current_y+1][current_x]=='.' || maze[current_y+1][current_x]=='x'){
-            current_y++;
-            direction = Direction::Down;
-        } else {
-            //If all other directions are invalid, go back
-            current_x++;
-            direction = Direction::Right;
-        }
-        //Change the maze to mark where I have been with an x
-        maze[current_y][current_x] = 'x';
-        //Print the new maze
-        printMaze(maze);
-    } else if (direction == Direction::Down) {
-        if (maze[current_y][current_x-1]=='.' || maze[current_y][current_x-1]=='x'){
-            current_x--;
-            direction = Direction::Left;
-        } else if (maze[current_y+1][current_x]=='.' || maze[current_y+1][current_x]=='x'){
-            current_y++;
-            direction = Direction::Down;
-        } else if (maze[current_y][current_x+1]=='.' || maze[current_y][current_x+1]=='x'){
-            current_x++;
-            direction = Direction::Right;
-        } else {
-            current_y--;
-            direction = Direction::Up;
-        }
-        maze[current_y][current_x] = 'x';
-        printMaze(maze);
-    } else if (direction == Direction::Right) {
-        if (maze[current_y+1][current_x]=='.' || maze[current_y+1][current_x]=='x'){
-            current_y++;
-            direction = Direction::Down;
-        } else if (maze[current_y][current_x+1]=='.' || maze[current_y][current_x+1]=='x'){
-            current_x++;
-            direction = Direction::Right;
-        } else if (maze[current_y-1][current_x]=='.' || maze[current_y-1][current_x]=='x'){
-            current_y--;
-            direction = Direction::Up;
-        } else {
-            current_x--;
-            direction = Direction::Left;
-        }
-        maze[current_y][current_x] = 'x';
-        printMaze(maze);
-    } else if (direction == Direction::Up) {
-        if (maze[current_y][current_x+1]=='.' || maze[current_y][current_x+1]=='x'){
-            current_x++;
-            direction = Direction::Right;
-        } else if (maze[current_y-1][current_x]=='.' || maze[current_y-1][current_x]=='x'){
-            current_y--;
-            direction = Direction::Up;
-        } else if (maze[current_y][current_x-1]=='.' || maze[current_y][current_x-1]=='x'){
-            current_x--;
-            direction = Direction::Left;
-        } else {
-            current_y++;
-            direction = Direction::Down;
-        }
-        maze[current_y][current_x] = 'x';
-        printMaze(maze);
+    //Depending on the current direction, the relative directions are computed:
+    Direction rightDir, forwardDir, leftDir, backDir;
+    if (direction == Direction::Up) {
+        rightDir   = Direction::Right; //Example, right with respect to up is right
+        forwardDir = Direction::Up;
+        leftDir    = Direction::Left;
+        backDir    = Direction::Down;
+    }
+    else if (direction == Direction::Right) {
+        rightDir   = Direction::Down; //Example, right with respect to right would be down
+        forwardDir = Direction::Right;
+        leftDir    = Direction::Up;
+        backDir    = Direction::Left;
+    }
+    else if (direction == Direction::Down) {
+        rightDir   = Direction::Left;
+        forwardDir = Direction::Down;
+        leftDir    = Direction::Right;
+        backDir    = Direction::Up;
+    }
+    else if (direction == Direction::Left) {
+        rightDir   = Direction::Up;
+        forwardDir = Direction::Left;
+        leftDir    = Direction::Down;
+        backDir    = Direction::Right;
     } else {
         //If no direction, then show error
         std::cout << "ERROR, direction not found";
     }
-    //If we are at the end (hardcoded), display solved
+
+    //Loop over all 4 possible directions and check if they are movable:
+    //(in the order of right, forward, left and otherwise go back)
+    Direction directions[4] = { rightDir, forwardDir, leftDir, backDir };
+    for (int i = 0; i < 4; i++) {
+        //Create new coordinate variables that we can freely change without changing current coordinates
+        int newY = current_y;
+        int newX = current_x;
+
+        //Convert direction to movement
+        //(0,0 is left top, so going up is negative y, going right is positive x etc.)
+        if (directions[i] == Direction::Up) {
+            newY--; //change the relevant coordinate by 1 in the correct direction
+        }
+        else if (directions[i] == Direction::Down) {
+            newY++;
+        }
+        else if (directions[i] == Direction::Left) {
+            newX--;
+        }
+        else if (directions[i] == Direction::Right) {
+            newX++;
+        }
+
+        //Check if that spot is a movable spot (either . or x)
+        if (maze[newY][newX] == '.' || maze[newY][newX] == 'x') {
+            //If the spot is found, we move there, so the current coordinates are adjusted and the direction is adjusted.
+            current_y = newY;
+            current_x = newX;
+            direction = directions[i];
+            break; //And after the first spot we found we no longer have to look, so we break.
+        }
+    }
+    //Now we change the character to show we have moved to this spot and we print the new maze
+    maze[current_y][current_x] = 'x';
+    printMaze(maze);
+
+    //If we are at the end (hardcoded coordinates), display solved
     if (current_x == 0 && current_y == 2) {
         std::cout << "The maze is solved!" << std::endl;    
     } else {
