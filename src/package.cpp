c6aab5582cfbf3ce09afc9f540d20ff7001371b3
Dembolesizwe <lukedelmulle@gmail.com>
2026-03-09 11:56:34 +0100

    Finished package

--- src/package.cpp
+++ src/package.cpp
@@ -12,17 +12,64 @@ class Package {
         
     
     public:
-        void setSenderName(const std::string& nameSender);
-        std::string setSenderName();
-        void setRecipientName(const std::string& nameRecipient);
-        std::string setRecipientName();
-        void setSenderAddress(const std::string& addressSender);
-        std::string setSenderAddress();
-        void setRecipientAddress(const std::string& addressRecipient);
-        std::string setRecipientAddress();
-        void setWeight(float weightPackage);
-        float setWeight();
-        void printInfo();
+
+        //Set the name of the sender
+        void Package::setSenderName(const std::string& nameSender){
+            senderName = nameSender;
+        }
+
+        //Get the name of the sender
+        std::string Package::getSenderName(){
+            return senderName;
+        }
+
+        //Set the name of the recipient
+        void Package::setRecipientName(const std::string& nameRecipient){
+            recipientName = nameRecipient;
+        }
+
+        //Get the name of the recipient
+        std::string Package::getRecipientName(){
+            return recipientName;
+        }
+
+        //Set the address of the sender
+        void Package::setSenderAddress(const std::string& addressSender){
+            senderAddress = addressSender;
+        }
+
+        //Get the address of the sender
+        std::string Package::getSenderAddress(){
+            return senderAddress;
+        }
+
+        //Set the address of the recipient
+        void Package::setRecipientAddress(const std::string& addressRecipient){
+            recipientAddress = addressRecipient;
+        }
+
+        //Get the address of the recipient
+        std::string Package::getRecipientAddress(){
+            return recipientAddress;
+        }
+
+        //Set the weight of the package
+        void Package::setWeight(float weightPackage){
+            weight = weightPackage;
+        }
+
+        //Get the weight of the package
+        float Package::getWeight(){
+            return weight;
+        }
+
+
+        void Package::printInfo(){
+            std::cout << "Sender: " << senderName << ", Sender Address: " << senderAddress << std::endl;
+            std::cout << "Recipient: " << recipientName << ", Recipient Address: " << recipientAddress << std::endl;
+            std::cout << "Weight: " << weight << " kg" << std::endl;
+        }
+
         float calculateCost(float weightPackage);
 
 
